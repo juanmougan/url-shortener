c752e98d77c7312ea65d68cc371bfa16fdb05fd7
Juan Manuel Mougan <juanmougan@gmail.com>
2020-04-08 23:28:06 -0300

    Receiving the URL from command line

--- src/manual_test.cr
+++ src/manual_test.cr
@@ -1,7 +1,10 @@
 require "./manual_test.cr"
 
+if ARGV.size < 1
+  raise "Need a URL to shorten"
+end
 shortened = UrlShortener::Shortener
-  .from_url("www.ole.com.ar")
+  .from_url(ARGV[0])
   .with_provider(UrlShortener::TinyUrlProvider.new)
   .shorten
 
